d38e80d804b13f14fec7868862fd4cce2e5b1d62
Hamza Razzaq <33603492+hamzarz@users.noreply.github.com>
2019-04-01 12:10:27 +0500

    Update actions.js

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,7 +1,9 @@
 debugger
 export const increment = () => async dispatch => {
+    //you can add async actions here
     dispatch({type:'INCREMENT'})
 }
 export const decrement = () => async dispatch => {
+    //you can add async actions here
     dispatch({type:'DECREMENT'})
 }
